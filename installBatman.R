3a308cc230c871a3a420e8774879ccac4042fe42
Joe Wandy <joe.wandy@glasgow.ac.uk>
2016-10-07 11:16:37 +0100

    getting a bit ridiculous

--- installBatman.R
+++ installBatman.R
@@ -1,3 +1,3 @@
 install.packages('batman', repos='http://R-Forge.R-project.org')
-install.packages('Rmpi')
-install.packages('coda')
+install.packages('Rmpi', repos='http://cran.rstudio.com/')
+install.packages('coda', repos='http://cran.rstudio.com/')
