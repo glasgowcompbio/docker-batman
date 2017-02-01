ea1ce8846383b2a5b47a7ac55870e63d37013798
Joe Wandy <joe.wandy@glasgow.ac.uk>
2017-02-01 17:09:51 +0000

    Updated the codes to support batch processing of spectra.

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 CODE_DIR="/home/rstudio/codes/notebooks"
-OUTPUT_FILE="/home/rstudio/NMR/results/diagnostic.html"
-TEMP_DIR="/home/rstudio/NMR/temp"
 
 /usr/games/cowsay 'Starting analysis'
-
-jupyter nbconvert --to HTML --ExecutePreprocessor.timeout=-1 --execute ${CODE_DIR}/predict_spectra_concentrations.ipynb --output ${OUTPUT_FILE} && rm -rf ${TEMP_DIR} && figlet 'FINISH'
+echo
+python ${CODE_DIR}/start_analysis.py && figlet 'FINISH'
--- start_docker_shell.sh
+++ start_docker_shell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 CODE_DIR='/Users/joewandy/git/pyBatman'
-NMR_DIR='/Users/joewandy/Dropbox/Meta_clustering/NMR/test'
+NMR_DIR='/Users/joewandy/Dropbox/Meta_clustering/NMR/test/Curve_study'
 
-docker run -v ${CODE_DIR}:/home/rstudio/codes -v ${NMR_DIR}:/home/rstudio/NMR -it -p 8787:8787 -p 9999:9999 batman bash
+docker run --rm -v ${CODE_DIR}:/home/rstudio/codes -v ${NMR_DIR}:/home/rstudio/NMR --name docker-batman -it -p 8787:8787 -p 9999:9999 joewandy/docker-batman:latest bash
