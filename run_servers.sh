2c76be1d92018a5102319af1a2bcb0e6fb4de6c9
Joe Wandy <joe.wandy@glasgow.ac.uk>
2018-03-05 14:44:14 +0000

    Add allow-root option

--- run_servers.sh
+++ run_servers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 /usr/lib/rstudio-server/bin/rserver --server-app-armor-enabled=0
-jupyter notebook --port=9999 --no-browser --ip=0.0.0.0
+jupyter notebook --ip=0.0.0.0 --port=9999 --no-browser --allow-root
--- start_docker_shell.sh
+++ start_docker_shell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 CODE_DIR='/Users/joewandy/git/pyBatman'
-NMR_DIR='/Users/joewandy/Dropbox/Meta_clustering/NMR/test/Curve_study'
+NMR_DIR='/Users/joewandy/Dropbox/Analysis/NMR/promastigotes/'
 
 docker run --rm -v ${CODE_DIR}:/home/rstudio/codes -v ${NMR_DIR}:/home/rstudio/NMR --name docker-batman -it -p 8787:8787 -p 9999:9999 joewandy/docker-batman:latest bash
